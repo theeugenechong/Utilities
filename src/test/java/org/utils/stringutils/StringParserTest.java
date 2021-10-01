544b2286d6bc198bc365b40974b46055e3ec98ca
theeugenechong <eugenechong20011@yahoo.com>
2021-10-01 19:44:56 +0800

    Add test class for StringParser

---
+++ src/test/java/org/utils/stringutils/StringParserTest.java
@@ -0,0 +1,41 @@
+package org.utils.stringutils;
+
+import org.junit.jupiter.api.Test;
+import org.utils.exceptions.InvalidStringInputException;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringParserTest {
+
+    // methodBeingTested_inputGiven_expectedOutcome
+    // Input, one separator, two parts - Happy path (positive test case)
+    @Test
+    void splitString_oneSeparator_expectTwoParts() throws InvalidStringInputException {
+        // Setup
+        String inputString = "Hello|world";
+        StringParser sp = new StringParser();
+        String separator = "\\|";
+        ArrayList<String> stringParts = sp.splitString(inputString, separator);
+
+        // Comparing actual and expected output
+        assertEquals(2, stringParts.size());
+    }
+
+    // Failure (negative) test case
+    // null input, expect exception
+    @Test
+    void splitString_nullInput_expectException() {
+        // Setup
+        String inputString = null;
+        StringParser sp = new StringParser();
+        String separator = "\\|";
+        // ArrayList<String> stringParts = sp.splitString(inputString, separator);
+
+        // Comparing actual and expected output
+        assertThrows(InvalidStringInputException.class,
+                ()-> sp.splitString(inputString, separator));
+        // assertEquals(0, stringParts.size());
+    }
+}
